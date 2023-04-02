830b3047d0bb73c37b75c4f43e5c143021f1dbb0
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-02 19:52:40 +0530

    let c

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,23 @@
+import GoLinkExternal from 'react-icons/go'
+
 const ProjectCard = ({
     imgSrc,
     projectName,
     projectLangs,
     projectDescription,
+    projectLink,
 }) => {
     return (
         <div className="project-card">
-            <div className="project-card-img">
-                <img src={imgSrc} alt={projectName} />
-            </div>
+            <a href={projectLink} target="_blank" rel="noopener noreferrer">
+                <div className="project-card-img">
+                    <img src={imgSrc} alt={projectName} />
+                </div>
+            </a>
             <div className="project-card-text">
-                <h2>{projectName}</h2>
+                <h2>
+                    {projectName} <GoLinkExternal />
+                </h2>
                 <div className="project-lang">
                     <p>{projectLangs}</p>
                 </div>
@@ -31,18 +38,21 @@ export const Projects = () => {
                     projectName="Elpha"
                     projectLangs="Node, discord.js, MongoDB"
                     projectDescription="Elpha is a verified bot made with discord.js and hosted on Railway. Its development aims to make server moderation easier."
+                    projectLink="https://dsc.gg/elpha"
                 />
                 <ProjectCard
                     imgSrc="https://img.itch.zone/aW1nLzY1OTc2NDAucG5n/original/MjFBYb.png"
                     projectName="Dark Forest"
                     projectLangs="C#, Unity Engine"
                     projectDescription="Dark Forest is a fps game made with C# and Unity Engine."
+                    projectLink="https://itch.io/pranshu05/game"
                 />
                 <ProjectCard
                     imgSrc="https://images-ext-1.discordapp.net/external/onODAhJidPfekpmsRhD2NuUVhGwLvMVOOCk0HRAK8Go/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/1079000764204204052/637bfc2c6f484c9d9e753a0a0243541b.webp?width=616&height=616"
                     projectName="Anti Ghost Ping"
                     projectLangs="Node, discord.js, MongoDB"
                     projectDescription="Powerful and open-source ghost ping detection bot! This bot can efficiently detect ghost pings in every channel of your server, and it even comes with a redirect message system. With this feature, you can easily keep track of all the ghost pings in your server without any hassle!"
+                    projectLink="https://dsc.gg/agp"
                 />
             </div>
         </div>
