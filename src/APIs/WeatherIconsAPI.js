686a7eedaa996f3000de6c2a85f614f0159c9f4a
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-05 14:05:29 +0530

    h

--- src/APIs/WeatherIconsAPI.js
+++ src/APIs/WeatherIconsAPI.js
@@ -1,32 +1,35 @@
 export function getWeatherIcon(weather) {
-    switch (weather) {
-        case 'Sunny':
-            return <span>☀️</span>
-        case 'Clear':
-            return <span>🌞</span>
-        case 'Partly cloudy':
-            return <span>⛅️</span>
-        case 'Cloudy':
-            return <span>☁️</span>
-        case 'Overcast':
-            return <span>🌥️</span>
-        case 'Mist':
-            return <span>🌫️</span>
-        case 'Patchy rain possible':
-            return <span>🌦️</span>
-        case 'Patchy snow possible':
-            return <span>🌨️</span>
-        case 'Patchy sleet possible':
-            return <span>🌨️</span>
-        case 'Patchy freezing drizzle possible':
-            return <span>🌨️</span>
-        case 'Thundery outbreaks possible':
-            return <span>🌩️</span>
-        case 'Blowing snow':
-            return <span>🌨️</span>
-        case 'Blizzard':
-            return <span>❄️</span>
-        default:
-            return null
+    if (weather.includes('Rain') && weather.includes('Thunder')) {
+        return <span>⛈️</span>
+    } else if (weather.includes('Thunder') && weather.includes('Snow')) {
+        return <span>🌨️🌩️</span>
+    } else if (weather.includes('Rain')) {
+        return <span>🌧️</span>
+    } else if (weather.includes('Thunder')) {
+        return <span>🌩️</span>
+    } else if (weather === 'Sunny') {
+        return <span>☀️</span>
+    } else if (weather === 'Clear') {
+        return <span>🌞</span>
+    } else if (weather === 'Partly cloudy') {
+        return <span>⛅️</span>
+    } else if (weather === 'Cloudy') {
+        return <span>☁️</span>
+    } else if (weather === 'Overcast') {
+        return <span>🌥️</span>
+    } else if (weather === 'Mist') {
+        return <span>🌫️</span>
+    } else if (weather.includes('Snow')) {
+        return <span>❄️</span>
+    } else if (weather === 'Fog') {
+        return <span>🌁</span>
+    } else if (weather === 'Hail') {
+        return <span>🌧️</span>
+    } else if (weather.includes('Showers')) {
+        return <span>🌧️</span>
+    } else if (weather.includes('Drizzle')) {
+        return <span>🌦️</span>
+    } else {
+        return null
     }
 }
