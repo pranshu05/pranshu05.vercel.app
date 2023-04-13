220c4c7370fc75b1f799ce889aa0bf59007537a4
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-13 10:23:35 +0530

    yes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,40 +1,23 @@
-import { Tech } from "./Technologies";
-import { useState, useEffect } from "react";
+import { Tech } from "./Technologies"
+import { useState, useEffect } from "react"
 
 export const AboutMe = () => {
-  const birthdate = new Date("2005-10-04");
-  const ageInYears = Math.floor(
-    (new Date() - birthdate) / (1000 * 60 * 60 * 24 * 365)
-  );
-  const ageInPoints = (new Date() - birthdate) / (1000 * 60 * 60 * 24 * 365);
-  const ageFormatted = ageInPoints.toFixed(7);
-
-  const [codingHours, setCodingHours] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://wakatime.com/api/v1/users/pranshu05/stats"
-      );
-      const data = await response.json();
-      setCodingHours(Math.floor(data.data.grand_total.total_seconds / 3600));
-    };
-    fetchData();
-  }, []);
+  const BIRTHDATE = new Date("2005-10-04")
+  const MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365
+  const ageInYears = Math.floor((new Date() - BIRTHDATE) / MS_PER_YEAR)
+  const ageInPoints = (new Date() - BIRTHDATE) / MS_PER_YEAR
+  const ageInYearsFormatted = ageInPoints.toFixed(9)
 
   return (
     <div className="about-me" id="about-me">
       <h2> Hi! I'm Pranshu ✌️</h2>
       <p>
-        I'm a {ageFormatted} y/o high school student and self-taught developer
-        from India. I started coding when I was 14 during the COVID lockdown and
-        have gained expertise in creating Discord bots using{" "}
+        I'm a {ageInYearsFormatted} year old high school student and self-taught
+        developer from India. I started coding when I was 14 during the COVID
+        lockdown and have gained expertise in creating Discord bots using{" "}
         <a href="https://discord.js.org/#/">discord.js</a> as well as web
         development. Along the way, I've learned various programming languages.
-        According to my Wakatime stats, I've spent {codingHours} hours coding so
-        far.
       </p>
-      <br />
       <h2>What Do I Do 💭</h2>
       <p>
         I am passionate about everything related to technology, including
@@ -44,9 +27,7 @@ export const AboutMe = () => {
         in these areas and apply it to gain a deeper understanding of the
         technology that surrounds us.
       </p>
-      <br />
       <Tech />
-      <br />
     </div>
-  );
-};
+  )
+}
