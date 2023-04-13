e8cff69d7117bf873a594aba9e12120404ccce11
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-13 10:11:56 +0530

    i

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,6 +6,7 @@ export const AboutMe = () => {
   const ageInYears = Math.floor(
     (new Date() - birthdate) / (1000 * 60 * 60 * 24 * 365.25)
   );
+  const ageFormatted = ageInYears.toFixed(7);
 
   const [codingHours, setCodingHours] = useState(0);
 
@@ -24,9 +25,9 @@ export const AboutMe = () => {
     <div className="about-me" id="about-me">
       <h2> Hi! I'm Pranshu ✌️</h2>
       <p>
-        I'm a {ageInYears} y/o high school student and self-taught
-        developer from India. I started coding when I was 14 during the COVID
-        lockdown and have gained expertise in creating Discord bots using{" "}
+        I'm a {ageFormatted} y/o high school student and self-taught developer
+        from India. I started coding when I was 14 during the COVID lockdown and
+        have gained expertise in creating Discord bots using{" "}
         <a href="https://discord.js.org/#/">discord.js</a> as well as web
         development. Along the way, I've learned various programming languages.
         According to my Wakatime stats, I've spent {codingHours} hours coding so
