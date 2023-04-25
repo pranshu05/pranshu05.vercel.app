4512b6c6040b0f01dd7dc6f22e6f350343f0c65d
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-25 10:22:25 +0530

    idk

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+const sendVisitorData = () => {
+    fetch('log/log-visitor.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+    })
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 )
+
+sendVisitorData()
