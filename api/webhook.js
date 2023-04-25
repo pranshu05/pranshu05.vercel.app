af02b89bebb61da767bc28c55a950bade5251bfb
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-25 11:30:55 +0530

    maxmind

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,5 @@
-const geoip = require('geoip-lite')
+const maxmind = require('maxmind')
+const fetch = require('node-fetch')
 
 const webhookHandler = async (req, res) => {
     try {
@@ -9,18 +10,21 @@ const webhookHandler = async (req, res) => {
         const WEBHOOK_URL =
             'https://discord.com/api/webhooks/1100272235740139610/iCHXpOVRBUmeMmIi5Zh1PAPZjfHQiVYKmHzkENYCxBOl2aLs-Uef0UngLvAgVJTj-1Qe'
 
+        // Load the GeoLite2-City database
+        const database = await maxmind.open('path/to/GeoLite2-City.mmdb')
+
         // Get the visitor's location and country flag
-        const geo = geoip.lookup(ip)
+        const geo = database.get(ip)
         if (!geo) {
             throw new Error(`Failed to lookup location for IP address ${ip}`)
         }
-        const flag = geo.country.toLowerCase()
+        const flag = geo.country.iso_code.toLowerCase()
 
         // Format the message
         const date = new Date().toLocaleDateString()
         const time = new Date().toLocaleTimeString()
         const location = req.headers.referer
-        const message = `${date}, ${time}, ${ip}, ${geo.city}, ${geo.region}, :flag_${flag}: --> ${location}`
+        const message = `${date}, ${time}, ${ip}, ${geo.city.names.en}, ${geo.subdivisions[0].names.en}, :flag_${flag}: --> ${location}`
 
         // Send the message to the Discord webhook using fetch
         await fetch(WEBHOOK_URL, {
