d27efd50ffc995a61e1f6f0b75eba37cfc992e21
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-25 11:24:34 +0530

    removed axios

--- api/webhook.js
+++ api/webhook.js
@@ -1,33 +1,41 @@
-const axios = require('axios')
 const geoip = require('geoip-lite')
 
 const webhookHandler = async (req, res) => {
-    // Get the visitor's IP address
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    try {
+        // Get the visitor's IP address
+        const ip =
+            req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
-    // Get the visitor's location and country flag
-    const geo = geoip.lookup(ip)
-    const flag = geo.country.toLowerCase()
+        const WEBHOOK_URL =
+            'https://discord.com/api/webhooks/1100272235740139610/iCHXpOVRBUmeMmIi5Zh1PAPZjfHQiVYKmHzkENYCxBOl2aLs-Uef0UngLvAgVJTj-1Qe'
 
-    // Format the message
-    const WEBHOOK_URL =
-        'https://discord.com/api/webhooks/1100272235740139610/iCHXpOVRBUmeMmIi5Zh1PAPZjfHQiVYKmHzkENYCxBOl2aLs-Uef0UngLvAgVJTj-1Qe'
-    const date = new Date().toLocaleDateString()
-    const time = new Date().toLocaleTimeString()
-    const location = req.headers.referer
-    const message = `${date}, ${time}, ${ip}, ${geo.city}, ${geo.region}, :flag_${flag}: --> ${location}`
+        // Get the visitor's location and country flag
+        const geo = geoip.lookup(ip)
+        if (!geo) {
+            throw new Error(`Failed to lookup location for IP address ${ip}`)
+        }
+        const flag = geo.country.toLowerCase()
 
-    // Send the message to the Discord webhook
-    await axios
-        .post(WEBHOOK_URL, { content: message })
-        .then((response) => {
-            console.log('Message sent to Discord')
+        // Format the message
+        const date = new Date().toLocaleDateString()
+        const time = new Date().toLocaleTimeString()
+        const location = req.headers.referer
+        const message = `${date}, ${time}, ${ip}, ${geo.city}, ${geo.region}, :flag_${flag}: --> ${location}`
+
+        // Send the message to the Discord webhook using fetch
+        await fetch(WEBHOOK_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ content: message }),
         })
-        .catch((error) => {
-            console.log('Error sending message to Discord', error)
-        })
-
-    res.status(200).send('Webhook triggered successfully')
+        console.log('Message sent to Discord')
+        res.status(200).send('Webhook triggered successfully')
+    } catch (error) {
+        console.log('Error sending message to Discord', error)
+        res.status(500).send('Failed to trigger webhook')
+    }
 }
 
 export default webhookHandler
