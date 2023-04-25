12a8dd8c92a30b15c3c782cfbfde77ff70c81fb2
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-25 11:42:37 +0530

    shit

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,4 @@
-const maxmind = require('maxmind')
+const fetch = require('node-fetch')
 
 const webhookHandler = async (req, res) => {
     try {
@@ -8,18 +8,10 @@ const webhookHandler = async (req, res) => {
         const WEBHOOK_URL =
             'https://discord.com/api/webhooks/1100272235740139610/iCHXpOVRBUmeMmIi5Zh1PAPZjfHQiVYKmHzkENYCxBOl2aLs-Uef0UngLvAgVJTj-1Qe'
 
-        const database = await maxmind.open('path/to/GeoLite2-City.mmdb')
-
-        const geo = database.get(ip)
-        if (!geo) {
-            throw new Error(`Failed to lookup location for IP address ${ip}`)
-        }
-        const flag = geo.country.iso_code.toLowerCase()
-
         const date = new Date().toLocaleDateString()
         const time = new Date().toLocaleTimeString()
         const location = req.headers.referer
-        const message = `${date}, ${time}, ${ip}, ${geo.city.names.en}, ${geo.subdivisions[0].names.en}, :flag_${flag}: --> ${location}`
+        const message = `${date}, ${time}, ${ip}, ${location}`
 
         await fetch(WEBHOOK_URL, {
             method: 'POST',
