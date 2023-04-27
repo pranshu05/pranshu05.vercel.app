88d34a71b89e291202307ad2ec08b1bcf2ae3796
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-27 07:16:02 +0530

    y

--- src/App.js
+++ src/App.js
@@ -7,8 +7,21 @@ import { LinksPage } from './Pages/Linkspage'
 import { MusicPage } from './Pages/Musicpage'
 import { Route, Routes } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+    useEffect(() => {
+        axios
+            .get('https://pranshu05.vercel.app/api/webhook')
+            .then((response) => {
+                console.log('Webhook triggered successfully')
+            })
+            .catch((error) => {
+                console.log('Error triggering webhook', error)
+            })
+    }, [])
+
     return (
         <div className="App">
             <BrowserRouter>
