69b3b53452cbf548d0a444145ef231552e620afe
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-27 18:36:44 +0530

    bk

--- src/components/Anonymous.js
+++ src/components/Anonymous.js
@@ -7,6 +7,7 @@ const AnonymousMessage = () => {
     const [isSuccess, setIsSuccess] = useState(false)
     const [isError, setIsError] = useState(false)
     const [location, setLocation] = useState(null)
+    const [messages, setMessages] = useState([])
 
     const DISCORD_WEBHOOK_URL =
         'https://discord.com/api/webhooks/1101001754281254943/LC3tzwvPVngBYbhYa4lrfGQ6fmTgeGCOiPsncK1L77Ej6hae_T_YK0HHaOyUUSqJf3NN'
@@ -35,43 +36,64 @@ const AnonymousMessage = () => {
         e.preventDefault()
         try {
             const small_code = location.country_code.toLowerCase()
-            const content = `${message}\n\nIP: ${location.ip}\nCity: ${location.city}\nCountry: ${location.country_name} (:flag_${small_code})`
+            const content = `\`\`\`${message}\`\`\`\n${location.ip} [${location.city},${location.country_name} :flag_${small_code}]`
             const response = await axios.post(DISCORD_WEBHOOK_URL, {
                 content: content,
             })
             console.log(response.data)
             setIsSuccess(true)
+            setMessages([
+                ...messages,
+                {
+                    type: 'success',
+                    message: 'Your message was sent successfully!',
+                },
+            ])
         } catch (error) {
             console.error(error)
             setIsError(true)
+            setMessages([
+                ...messages,
+                {
+                    type: 'error',
+                    message:
+                        'Sorry, there was an error sending your message. Please try again later.',
+                },
+            ])
         }
     }
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (messages.length > 0) {
+                setMessages(messages.slice(1))
+            }
+        }, 2000)
+        return () => clearTimeout(timeout)
+    }, [messages])
 
     return (
         <div>
             <div className="ano-cont">
-                <form onSubmit={handleSubmit}>
-                    <div className="ano-form-cont">
-                        <textarea
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
-                            placeholder="Send anonymous message"
-                        />
-                    </div>
-                    <div className="ano-button-cont">
-                        <button type="submit">
-                            <TbSend />
-                        </button>
-                    </div>
-                </form>
+                <div className="ano-form-cont">
+                    <textarea
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="Send anonymous message"
+                    />
+                </div>
+                <div className="ano-button-cont">
+                    <TbSend onClick={handleSubmit} />
+                </div>
             </div>
-            {isSuccess && <p>Your message was sent successfully!</p>}
-            {isError && (
-                <p>
-                    Sorry, there was an error sending your message. Please try
-                    again later.
-                </p>
-            )}
+            {messages.map((msg, index) => (
+                <div
+                    key={index}
+                    className={msg.type === 'success' ? 'success' : 'error'}
+                >
+                    <p>{msg.message}</p>
+                </div>
+            ))}
         </div>
     )
 }
