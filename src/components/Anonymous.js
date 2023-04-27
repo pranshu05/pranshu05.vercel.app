a64527532bfab56ce1fc99b04a218a10af8d33d4
Pranshu Patel <pranshu05patel@gmail.com>
2023-04-27 11:58:12 +0530

    ieygfprw

--- src/components/Anonymous.js
+++ src/components/Anonymous.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { TbSend } from 'react-icons/tb'
 
@@ -6,15 +6,38 @@ const AnonymousMessage = () => {
     const [message, setMessage] = useState('')
     const [isSuccess, setIsSuccess] = useState(false)
     const [isError, setIsError] = useState(false)
+    const [location, setLocation] = useState(null)
 
     const DISCORD_WEBHOOK_URL =
         'https://discord.com/api/webhooks/1101001754281254943/LC3tzwvPVngBYbhYa4lrfGQ6fmTgeGCOiPsncK1L77Ej6hae_T_YK0HHaOyUUSqJf3NN'
 
+    useEffect(() => {
+        const fetchLocation = async () => {
+            try {
+                const response = await fetch(
+                    'https://api.ipify.org/?format=json'
+                )
+                const data = await response.json()
+                const ip = data.ip
+                const locationResponse = await fetch(
+                    `https://ipapi.co/${ip}/json/`
+                )
+                const locationData = await locationResponse.json()
+                setLocation(locationData)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchLocation()
+    }, [])
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
+            const small_code = location.country_code.toLowerCase()
+            const content = `${message}\n\nIP: ${location.ip}\nCity: ${location.city}\nCountry: ${location.country_name} (:flag_${small_code})`
             const response = await axios.post(DISCORD_WEBHOOK_URL, {
-                content: message,
+                content: content,
             })
             console.log(response.data)
             setIsSuccess(true)
