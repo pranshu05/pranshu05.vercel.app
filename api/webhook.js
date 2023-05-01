9ac531d66a4105a6872583a42055e7621b8fcca7
Pranshu Patel <pranshu05patel@gmail.com>
2023-05-01 23:20:41 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,32 +1,30 @@
+  
 const webhookHandler = async (req, res) => {
-   try {
-      const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    try {
+        const ip =
+            req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
-      const WEBHOOK_URL = process.env.REACT_APP_WEBHOOK_URL
+        const WEBHOOK_URL = process.env.REACT_APP_WEBHOOK_URL
 
-      const date = new Date().toLocaleDateString()
-      const time = new Date().toLocaleTimeString()
+        const date = new Date().toLocaleDateString()
+        const time = new Date().toLocaleTimeString()
 
-      const locationResponse = await fetch(`https://ipapi.co/${ip}/json/`)
-      const locationData = await locationResponse.json()
-      const { country_name, country_code, city } = locationData
-      const small_code = country_code.toLowerCase()
+        const locationResponse = await fetch(`https://ipapi.co/${ip}/json/`)
+        const locationData = await locationResponse.json()
+        const { country_name, country_code, city } = locationData
+        const small_code = country_code.toLowerCase()
 
-      const refererUrl = req.headers.referer
-      const origin = refererUrl ? new URL(refererUrl).origin : 'unknown'
-      const message = `\`${date} ${time} (${ip})\` [${origin}] \n[${city}, ${country_name} :flag_${small_code}:]`
+        const location = req.headers.referer
+        const message = `\`${date} ${time} (${ip})\` [${location}] \n [${city}, ${country_name} :flag_${small_code}:]`
 
-      await fetch(WEBHOOK_URL, {
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/json',
-         },
-         body: JSON.stringify({ content: message }),
-      })
-      res.status(200).send('Webhook triggered successfully')
-   } catch (error) {
-      res.status(500).send('Failed to trigger webhook')
-   }
+        await fetch(WEBHOOK_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ content: message }),
+        })
+    } catch (error) {}
 }
 
 export default webhookHandler
