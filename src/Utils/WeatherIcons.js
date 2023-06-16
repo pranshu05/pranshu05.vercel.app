950aae1179117e95edbcd215867791823f825745
Pranshu Patel <pranshu05patel@gmail.com>
2023-06-16 18:42:14 +0530

    weather icons oof

--- src/Utils/WeatherIcons.js
+++ src/Utils/WeatherIcons.js
@@ -1,27 +1,48 @@
 export function getWeatherIcon(weather) {
-   if (weather.includes('Sunny')) {
-      return <span>☀️</span>;
-   } else if (weather.includes('Clear')) {
-      return <span>🌞</span>;
-   } else if (weather.includes('Partly cloudy')) {
-      return <span>⛅️</span>;
-   } else if (weather.includes('Cloudy')) {
-      return <span>☁️</span>;
-   } else if (weather.includes('Overcast')) {
-      return <span>🌥️</span>;
-   } else if (weather.includes('Mist') || weather.includes('Foggy')) {
-      return <span>🌫️</span>;
-   } else if (weather.includes('Smoke')) {
-      return <span>🌫️</span>;
-   } else if (weather.includes('Rain') || weather.includes('Shower') || weather.includes('Drizzle')) {
-      return <span>🌧️</span>;
-   } else if (weather.includes('Snow') || weather.includes('Blizzard') || weather.includes('Sleet')) {
-      return <span>❄️</span>;
-   } else if (weather.includes('Thunder') || weather.includes('Storm')) {
-      return <span>🌩️</span>;
-   } else if (weather.includes('Hail')) {
-      return <span>🌧️</span>;
-   } else {
-      return null;
+   switch (weather) {
+      case 'Sunny':
+         return <span>☀️</span>
+      case 'Clear':
+         return <span>🌞</span>
+      case 'Partly cloudy':
+         return <span>⛅️</span>
+      case 'Cloudy':
+         return <span>☁️</span>
+      case 'Overcast':
+         return <span>🌥️</span>
+      case 'Mist':
+         return <span>🌫️</span>
+      case 'Patchy rain possible':
+         return <span>🌦️</span>
+      case 'Patchy snow possible':
+         return <span>🌨️</span>
+      case 'Patchy sleet possible':
+         return <span>🌨️</span>
+      case 'Moderate or heavy rain with thunder':
+         return <span>🌩️</span>
+      case 'Patchy freezing drizzle possible':
+         return <span>🌨️</span>
+      case 'Thundery outbreaks possible':
+         return <span>🌩️</span>
+      case 'Blowing snow':
+         return <span>🌨️</span>
+      case 'Blizzard':
+         return <span>❄️</span>
+      case 'Fog':
+         return <span>🌁</span>
+      case 'Hail':
+         return <span>🌧️</span>
+      case 'Heavy rain':
+         return <span>🌧️</span>
+      case 'Heavy snow':
+         return <span>❄️</span>
+      case 'Heavy sleet':
+         return <span>🌨️</span>
+      case 'Heavy showers':
+         return <span>🌧️</span>
+      case 'Thunderstorms':
+         return <span>🌩️</span>
+      default:
+         return null
    }
 }
