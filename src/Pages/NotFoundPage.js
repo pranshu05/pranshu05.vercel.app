7bdfa40ee03192e3256dc6c56a4d249b8682d2db
Pranshu Patel <pranshu05patel@gmail.com>
2023-09-30 17:05:37 +0530

    Images

--- src/Pages/NotFoundPage.js
+++ src/Pages/NotFoundPage.js
@@ -4,9 +4,9 @@ const NotFoundPage = () => {
    return (
       <div className="not-page-cont">
          <div className="not-page-all">
-            <h2>404 Page Not Found</h2>
+            <h1>404 Page Not Found</h1>
             <img
-               src="https://seranking.com/blog/wp-content/uploads/2021/01/404_01-min.jpg"
+               src="https://github.com/pranshu05/pranshu05.vercel.app/assets/70943732/ecd7bd4a-5a76-4cbd-bb55-1b6e776b3659"
                alt=""
             />
             <p>
