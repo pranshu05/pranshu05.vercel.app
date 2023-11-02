9f9ce03752f6766fc9b7f1a35c55b5dd3412787a
Pranshu Patel <pranshu05patel@gmail.com>
2023-11-02 10:51:19 +0530

    idk

--- src/Pages/ImageGalleryPage.js
+++ src/Pages/ImageGalleryPage.js
@@ -1,35 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { NavBar } from '../components/NavBar'
 import { ImageGallery } from '../components/ImageGallery'
 import { Footer } from '../components/Footer'
-import axios from 'axios'
-
 export const ImageGalleryPage = () => {
     const [isModelOpen, setIsModelOpen] = useState(false)
-    const [profileInfo, setProfileInfo] = useState({})
-
-    useEffect(() => {
-        const fetchProfileInfo = async () => {
-            try {
-                const response = await axios.get(
-                    'https://api.unsplash.com/users/pranshu05',
-                    {
-                        params: {
-                            client_id:
-                                'IpuBMtdoSBFo8bS7L1gevS7rRFBdEDN9Wp7du9QFh1A',
-                        },
-                    }
-                )
-
-                setProfileInfo(response.data)
-            } catch (error) {
-                console.error('Error fetching profile information:', error)
-            }
-        }
-
-        fetchProfileInfo()
-    }, [])
 
     return (
         <div>
@@ -40,13 +15,14 @@ export const ImageGalleryPage = () => {
                 </p>
                 <h1>My Photography</h1>
                 <p>
+                    Visit my
                     <a
                         href={`https://unsplash.com/@pranshu05`}
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         {' '}
-                        Visit my Unsplash Profile{' '}
+                        Unsplash Profile{' '}
                     </a>{' '}
                 </p>
                 <ImageGallery onModelOpen={setIsModelOpen} />
