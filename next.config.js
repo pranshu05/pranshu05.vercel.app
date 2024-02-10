9955298503e3d2848343e37a3c2f1bf664708c11
Pranshu Patel <pranshu05patel@gmail.com>
2024-02-10 23:24:57 +0530

    Remove unused code in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [],
-        rehypePlugins: [],
-    },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
@@ -14,5 +6,3 @@ const nextConfig = {
         mdxRs: true
     }
 }
-
-module.exports = withMDX(nextConfig)
