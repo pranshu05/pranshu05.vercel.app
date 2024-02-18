95288bb480eaa1cebf1787dde318f3bea947d2f7
Pranshu Patel <pranshu05patel@gmail.com>
2024-02-18 19:46:39 +0530

    Add fullscreen functionality to ImageGallery component and update styling in NavBar component

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,26 +1,39 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { MdNavigateBefore, MdNavigateNext, MdOutlineClose } from "react-icons/md";
 
 interface Image {
-    urls: { regular: string; };
+    urls: { regular: string };
 }
 
 const ImageGallery: React.FC = () => {
     const [imageData, setImageData] = useState<Image[]>([]);
+    const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null);
 
     const fetchImagesFromUnsplash = async () => {
         try {
             const response = await axios.get('https://api.unsplash.com/users/pranshu05/photos', {
-                params: {
-                    client_id: 'IpuBMtdoSBFo8bS7L1gevS7rRFBdEDN9Wp7du9QFh1A',
-                    per_page: 30,
-                },
+                params: { client_id: 'IpuBMtdoSBFo8bS7L1gevS7rRFBdEDN9Wp7du9QFh1A', per_page: 30 },
             });
-
             setImageData(response.data);
         } catch (error) {
             console.error('Error fetching images:', error);
+        }
+    };
+
+    const openFullscreen = (index: number) => {
+        setSelectedImageIndex(index);
+    };
+
+    const closeFullscreen = () => setSelectedImageIndex(null);
+
+    const navigateImage = (step: number) => {
+        if (selectedImageIndex !== null) {
+            const newIndex = selectedImageIndex + step;
+            if (newIndex >= 0 && newIndex < imageData.length) {
+                setSelectedImageIndex(newIndex);
+            }
         }
     };
 
@@ -29,12 +42,22 @@ const ImageGallery: React.FC = () => {
     }, []);
 
     return (
-        <div className="columns-2 md:columns-3 gap:2 md:gap-4">
+        <div className="columns-2 md:columns-3 gap-2 md:gap-4">
             {imageData.map((image, index) => (
-                <div className="mb-2 md:mb-4" key={index}>
-                    <img className='h-auto max-w-full rounded-lg grayscale transition-all duration-300 ease-in-out transform hover:grayscale-0' src={image.urls.regular} alt="" loading="lazy" />
+                <div key={index} className='mb-2 md:mb-4' onClick={() => openFullscreen(index)}>
+                    <img className="w-full h-auto rounded-lg grayscale transition-all duration-300 ease-in-out transform hover:grayscale-0 cursor-pointer" src={image.urls.regular} alt="" loading="lazy" />
                 </div>
             ))}
+            {selectedImageIndex !== null && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop backdrop-blur-sm" onClick={closeFullscreen}>
+                    <div className="relative h-dvh w-dvw flex items-center">
+                        <img className="max-w-full max-h-full my-auto mx-auto rounded-lg" src={imageData[selectedImageIndex]?.urls.regular} alt="" />
+                        <button className="absolute p-4 left-0 top-1/2 transform -translate-y-1/2 cursor-pointer text-2xl" onClick={(e) => { e.stopPropagation(); navigateImage(-1); }}><MdNavigateBefore /></button>
+                        <button className="absolute p-4 right-0 top-1/2 transform -translate-y-1/2 cursor-pointer text-2xl" onClick={(e) => { e.stopPropagation(); navigateImage(1); }}><MdNavigateNext /></button>
+                        <button className="absolute top-0 right-0 p-4 cursor-pointer text-2xl" onClick={(e) => { e.stopPropagation(); closeFullscreen(); }}><MdOutlineClose /></button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -19,11 +19,11 @@ const NavBar: React.FC = () => {
                 <div className="flex gap-4">
                     {navRoutes.map((route) => (
                         <Link key={route} href={route} passHref>
-                            <p className={`text-white py-1 px-2 rounded-full ${path === route ? 'bg-black bg-opacity-50' : 'hover:bg-black hover:bg-opacity-40'}`} onClick={handleLinkClick} > {route === '/' ? '/' : route.substring(1)} </p>
+                            <p className={`text-white py-1 px-2 rounded-full ${path === route ? 'bg-zinc-300 bg-opacity-50' : 'hover:bg-zinc-400 hover:bg-opacity-40'}`} onClick={handleLinkClick} > {route === '/' ? '/' : route.substring(1)} </p>
                         </Link>
                     ))}
                     <div className="relative inline-block">
-                        <button type="button" className="inline-flex w-full justify-center gap-x-1.5 text-white py-1 px-2 rounded-full hover:bg-black hover:bg-opacity-40 transition" onClick={() => setIsOpen(!isOpen)}>
+                        <button type="button" className="inline-flex w-full justify-center gap-x-1.5 text-white py-1 px-2 rounded-full hover:bg-zinc-400 hover:bg-opacity-40 transition" onClick={() => setIsOpen(!isOpen)}>
                             more
                             <svg className="-mr-1 h-6 w-6 pt-1 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                 <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
