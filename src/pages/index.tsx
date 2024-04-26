9c6e25bfe31ab53500488f30c708feed8a61dbb6
Pranshu Patel <pranshu05patel@gmail.com>
2024-04-26 18:01:15 +0530

    Refactor HomeContainer import to use dynamic import in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
-import HomeContainer from "@/components/(home)/HomeContainer";
+import dynamic from "next/dynamic";
+
+const HomeContainer = dynamic(() => import("@/components/(home)/HomeContainer"), {
+    ssr: false,
+  });
 
 export const metadata: Metadata = {
     title: 'Pranshu05',
