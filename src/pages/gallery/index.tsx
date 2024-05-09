ade4219d4bd9409717b0eabb44071a04a102bebd
Pranshu Patel <pranshu05patel@gmail.com>
2024-05-09 15:06:03 +0530

    Refactor image loading and update image elements in components

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -2,7 +2,7 @@ import ImageGallery from '../../components/(gallery)/ImageGallery';
 
 const ImageGalleryPage: React.FC = () => {
     return (
-        <div className="w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 mx-auto">
+        <div data-aos="fade-up" className="w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 mx-auto">
             <div className="pb-8">
                 <h1 className="text-3xl font-bold text-zinc-100">Gallery</h1>
                 <p>Welcome to my Gallery! Visit my <a href='https://unsplash.com/@pranshu05' target="_blank" className='link'>Unsplash Profile</a></p>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -38,7 +38,7 @@ const Blog: React.FC<{ posts: Post[] }> = ({ posts }) => {
     }, [posts]);
 
     return (
-        <div className='w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 mx-auto'>
+        <div data-aos="fade-up" className='w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 mx-auto'>
             <BlogPageHeader />
             {Object.entries(postsByYear).map(([year, yearPosts]) => (
                 <div key={year}>
