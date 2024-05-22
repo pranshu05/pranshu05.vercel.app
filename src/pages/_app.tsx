d1c9f0fd3da6af8c913e7d2fecd7abbeb393f638
Pranshu Patel <pranshu05patel@gmail.com>
2024-05-22 11:42:15 +0530

    Add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { useEffect } from 'react'
+import { Space_Grotesk } from 'next/font/google'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import NavBar from "@/components/(layout)/NavBar";
 import Footer from "@/components/(layout)/Footer";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
+const font = Space_Grotesk({
+    weight: '400',
+    subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
     useEffect(() => {
@@ -19,7 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
     }, [])
 
     return (
-        <div className="min-h-dvh w-dvw p-0 m-0 bg-neutral-950 text-zinc-300 flex flex-col">
+        <div className={`min-h-dvh w-dvw p-0 m-0 bg-neutral-950 text-zinc-300 flex flex-col ${font.className}`}>
             <NavBar />
             <div className="flex-1">
                 <Component {...pageProps} />
