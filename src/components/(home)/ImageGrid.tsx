e3cb9453b371b001c9cf795984c7c3db829a72d7
Pranshu Patel <pranshu05patel@gmail.com>
2024-07-04 17:19:49 +0530

    h

--- src/components/(home)/ImageGrid.tsx
+++ src/components/(home)/ImageGrid.tsx
@@ -5,7 +5,7 @@ const HomeImageGrid: React.FC = () => (
     <div className="flex justify-center gap-5 overflow-hidden lg:overflow-visible py-4">
         {images.map((src, index) => (
             <div key={index} className={`w-36 md:w-44 lg:w-52 flex-none overflow-hidden rounded-lg ${index % 2 === 0 ? '-rotate-2' : 'rotate-2'}`}>
-                <img className="w-full h-auto rounded-lg grayscale" width={1000} height={1000} src={src} alt="" loading="lazy" />
+                <img className="w-full h-auto rounded-lg grayscale hover:grayscale-0" width={1000} height={1000} src={src} alt="" loading="lazy" />
             </div>
         ))}
     </div>
