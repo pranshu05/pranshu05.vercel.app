743cc9cc670bbaceecfc17b273a936184d433208
Pranshu Patel <pranshu05patel@gmail.com>
2024-10-09 15:07:57 +0530

    lol

--- src/components/(home)/HomeContainer.tsx
+++ src/components/(home)/HomeContainer.tsx
@@ -22,7 +22,7 @@ const HomeContainer: React.FC = () => {
                 </div>
                 <p className="text-zinc-400">{ageInYears}y/o Developer, India • {currentTime}</p>
             </div>
-            <p>Hey there! 👋🏻 I&apos;m Pranshu, a full-stack developer and uni sophomore based in India. Currently working on a few projects and pursuing B.Tech. in ICT from <a className="link" href="https://daiict.ac.in" target="_blank" rel="noopener noreferrer">DA-IICT</a>.</p>
+            <p>Hey there! 👋🏻 I&apos;m Pranshu, a full-stack developer and uni sophomore based in India. Currently working on a few projects and pursuing B.Tech. in Information and Communication Technology from <a className="link" href="https://daiict.ac.in" target="_blank" rel="noopener noreferrer">Dhirubhai Ambani Institute of Information and Communication Technology</a>.</p>
             <SocialLinksGrid />
             <p>In my free time, I like to click photos, some of them are showcased below. Check out my <Link className='link' href='/gallery'>gallery</Link> for more. (Checkout my <a href='https://unsplash.com/@pranshu05' target='_blank' className='link'>Unsplash profile</a>.)</p>
             <HomeImageGrid />
