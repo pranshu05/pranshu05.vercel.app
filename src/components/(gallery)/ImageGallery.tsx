59ddb878af70c5c44db98ae403e946b79b8566b8
Pranshu Patel <pranshu05patel@gmail.com>
2024-10-27 01:39:26 +0530

    refactor: Improve navigation in ImageGallery component

--- src/components/(gallery)/ImageGallery.tsx
+++ src/components/(gallery)/ImageGallery.tsx
@@ -46,8 +46,10 @@ const ImageGallery: React.FC = () => {
     };
 
     const navigateImage = (step: number) => {
-        const newIndex = selectedImageIndex !== null ? selectedImageIndex + step : null;
-        (newIndex !== null && newIndex >= 0 && newIndex < imageData.length) && setSelectedImageIndex(newIndex);
+        if (selectedImageIndex !== null) {
+            const newIndex = (selectedImageIndex + step + imageData.length) % imageData.length;
+            setSelectedImageIndex(newIndex);
+        }
     };
 
     useEffect(() => {
@@ -67,9 +69,9 @@ const ImageGallery: React.FC = () => {
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 backdrop backdrop-blur-md" onClick={closeFullscreen}>
                     <div className="relative h-dvh w-dvw flex items-center">
                         <img className="max-w-full max-h-full w-auto h-auto my-auto mx-auto rounded-lg" width={1000} height={1000} src={imageData[selectedImageIndex]?.urls.regular} alt="" />
-                        <button className="absolute p-2 left-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-2xl bg-zinc-900 rounded-lg" onClick={(e) => { e.stopPropagation(); navigateImage(-1); }}><MdNavigateBefore /></button>
-                        <button className="absolute p-2 right-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-2xl bg-zinc-900 rounded-lg" onClick={(e) => { e.stopPropagation(); navigateImage(1); }}><MdNavigateNext /></button>
-                        <button className="absolute p-2 top-2 right-2 cursor-pointer text-2xl bg-zinc-900 rounded-lg" onClick={(e) => { e.stopPropagation(); closeFullscreen(); }}><MdOutlineClose /></button>
+                        <button className="absolute left-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-2xl" onClick={(e) => { e.stopPropagation(); navigateImage(-1); }}><MdNavigateBefore /></button>
+                        <button className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-2xl" onClick={(e) => { e.stopPropagation(); navigateImage(1); }}><MdNavigateNext /></button>
+                        <button className="absolute top-2 right-2 cursor-pointer text-2xl" onClick={(e) => { e.stopPropagation(); closeFullscreen(); }}><MdOutlineClose /></button>
                     </div>
                 </div>
             )}
